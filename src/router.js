d6a12a6cf5daa7b3cc87f81884a234a4cf4743f0
Ingrid Jufkin <Ingrid@Ingrids-MBP.lan>
2021-05-05 23:01:13 +0300

    download files in pdf (tabel) and excel added

--- src/router.js
+++ src/router.js
@@ -10,7 +10,8 @@ const {
   deleteDoneTask,
   deleteTodoTask,
   deleteTask,
-  getTasksByName
+  getTasksByName,
+  downloadFile
 } = require("./controllers");
 
 router.get("/todo-tasks", getTodoTasks);
@@ -19,6 +20,8 @@ router.get("/all-tasks", getAllTasks);
 // id is mongo object _id and toTask is todo or done
 router.get("/moveTask/:id/:toTask", moveTask);
 router.post("/createTask", createTask);
+router.post("/downloadFile", downloadFile);
+
 router.delete("/:id/:toTask", deleteDoneTask);
 router.delete("/:id/:toTask", deleteTodoTask);
 router.delete("/deleteTask", deleteTask);
