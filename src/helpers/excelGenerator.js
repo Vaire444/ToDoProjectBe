896c1e917a1e8187a06661f4bb4c0fb797bdc0c5
*Vaire <*vaire481@gmail,com*>
2021-05-16 16:29:09 +0300

    pdfGenerator teamwork 15.05.21

--- src/helpers/excelGenerator.js
+++ src/helpers/excelGenerator.js
@@ -3,38 +3,58 @@ var xl = require("excel4node");
 module.exports = async function (result, res) {
   let wb = new xl.Workbook();
   let ws = wb.addWorksheet("Sheet 1");
+  
+ 
 
   const style = wb.createStyle({
     font: {
       color: "#000000",
       size: 12,
     },
-    numberFormat: "$#,##0.00; ($#,##0.00); -",
+    numberFormat: "0",
   });
 
   ws.column(1).setWidth(30);
 
-  ws.cell(1, 1).string("Todo").style(style);
-  ws.cell(1, 2).string("Status").style(style);
-  ws.cell(1, 3).string("Priority").style(style);
+  ws.cell(1, 1).string("Number").style(style);
+  ws.cell(1, 2).string("Todo").style(style);
+  ws.cell(1, 3).string("Status").style(style);
+  ws.cell(1, 4).string("Priority").style(style);
 
   let row = 1;
-
+  let todoStart = row +1;
+  
   for await (const item of result.todoTasks) {
     row++;
 
-    ws.cell(row, 1).string(item.title).style(style);
-    ws.cell(row, 2).string("Todo").style(style);
-    ws.cell(row, 3).string(item.priority.toLowerCase()).style(style);
+    ws.cell(row, 1).number(row-1).style(style);
+    ws.cell(row, 2).string(item.title).style(style);
+    ws.cell(row, 3).string("Todo").style(style);
+    ws.cell(row, 4).string(item.priority.charAt(0).toUpperCase() + item.priority.toLowerCase().slice(1)).style(style);
+  
   }
-
+  let todoEnd = row;
+  let doneStart = row+1;
+ 
   for await (const item of result.doneTasks) {
     row++;
+    ws.cell(row, 1).number(row-1).style(style);
+    ws.cell(row, 2).string(item.title).style(style);
+    ws.cell(row, 3).string("Done").style(style);
+    ws.cell(row, 4).string(item.priority.charAt(0).toUpperCase() + item.priority.toLowerCase().slice(1)).style(style);
+    }
 
-    ws.cell(row, 1).string(item.title).style(style);
-    ws.cell(row, 2).string("Done").style(style);
-    ws.cell(row, 3).string(item.priority.toLowerCase()).style(style);
+  let doneEnd = row;
+  if (doneEnd === todoEnd){
+    doneEnd = row+1;
   }
+  ws.cell(row+2, 1).string("Total Todo").style(style);
+  ws.cell(row+3, 1).string("Total Done").style(style);
+  ws.cell(row+4, 1).string("Total").style(style);
+
+  ws.cell(row+2,2).formula('SUM(A' + todoStart + ':A' +(todoEnd) + ')' ).style(style)
+  ws.cell(row+3,2).formula('SUM(A' + doneStart + ':A' +(doneEnd) + ')' ).style(style)
+  ws.cell(row+4,2).formula('SUM(A' + todoStart + ':A' +(doneEnd) + ')' ).style(style)
 
   wb.write("Excel.xlsx", res);
 };
--- src/helpers/pdfGenerator.js
+++ src/helpers/pdfGenerator.js
@@ -3,12 +3,15 @@ require("jspdf-autotable");
 
 module.exports = async function (result, res) {
   const doc = new jsPDF("p", "mm");
-
+  const doneLenght = result.doneTasks.length;
+  const todoLenght = result.todoTasks.length;
+ 
   let startX = 15;
   let startY = 20;
 
   const header = [
     { header: "Title", dataKey: "title" },
+    { header: "Date", dataKey: "date" },
     { header: "Priority", dataKey: "priority" },
   ];
 
@@ -30,10 +33,32 @@ module.exports = async function (result, res) {
     doc.text("Done", startX, startY);
     startY += 5;
 
-    doc.autoTable(header, result.doneTasks, {
+    const table = doc.autoTable(header, result.doneTasks, {
+      styles: { fillColor: [255, 0, 0] },
       startY,
+      didDrawPage(HookData) {
+        return HookData.table;
+      },
     });
+    startY = table.lastAutoTable.finalY + 16;
   }
+  
+
+    doc.text("Total", startX, startY);
+    startY += 5;
+
+    doc.autoTable({
+      columnStyles: { 0: { halign: 'center', fillColor: [0, 255, 0], } },
+       // Cells in first column centered and green
+      margin: { top: 10 },
+      head: [['Task', 'Total']],
+      body: [        
+        ['Todo', todoLenght],
+        ['Done', doneLenght],
+       ],
+    })
+   
+  
 
   res.setHeader(
     "Content-Disposition",
